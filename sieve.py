327f1a3b2812307e1abac801db679cb0684352a3
Soham Mirikar <82722112+sohamm20@users.noreply.github.com>
2023-10-25 21:54:43 +0530

    Update sieve.py

--- sieve.py
+++ sieve.py
@@ -1,24 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 
 class Primes:
 
     def __init__(self, n):
 
-        self.prime = [True for i in range(n+1)] 
+        self.prime = [True for i in range(n + 1)]
         p = 2
-        while (p * p <= n): 
-            if (self.prime[p]): 
-                for i in range(p * p, n+1, p): 
+        while p * p <= n:
+            if self.prime[p]:
+                for i in range(p * p, n + 1, p):
                     self.prime[i] = False
             p += 1
-            
+
     def getPrimes(self):
-        
+
         ans = []
 
         for i in range(1, len(self.prime)):
             if self.prime[i]:
                 ans.append(i)
-        
+
         return ans
 
     def isPrime(self, x):
@@ -26,5 +29,5 @@ class Primes:
 
 
 S = Primes(100)
-print(S.getPrimes())
-print(S.isPrime(97))
+print S.getPrimes()
+print S.isPrime(97)
