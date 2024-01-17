862316723c631e6bedf89733b7074d125ed070aa
Soham Mirikar <82722112+sohamm20@users.noreply.github.com>
2024-01-17 21:56:38 +0530

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -1,12 +1,10 @@
 def generate(a):
     n = len(a)
-    ans = list()
+    ans = [[] for i in range(1 << n)]
     for i in range(1 << n):
-        curr = list()
         for j in range(n):
             if i & (1 << j) != 0:
-                curr.append(a[j])
-        ans.append(curr)
+                ans[i].append(a[j])
     return ans
 
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
